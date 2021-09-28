49c26aea88640599c365c1d06a121e8f9c47570d
AtulKarn <45361490+AtulKarn@users.noreply.github.com>
2021-09-28 20:46:08 +0545

    Create 922. Sort Array By Parity II.cpp

---
+++ 922. Sort Array By Parity II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n=nums.size(), pos;
+        for(int i=0;i<n;i++){
+            pos = i+1;
+            if ((i%2==0) && (nums[i]%2!=0)){
+                while(pos<n-1 && nums[pos]%2!=0)
+                    pos+=2;
+                cout<<i<<" "<<pos<<" "<<nums[i]<<" even\n";
+                int temp=nums[i];
+                nums[i]=nums[pos];
+                nums[pos]=temp;
+            }
+            if ((i%2!=0) && (nums[i]%2==0)){
+                while(pos<n-1 && nums[pos]%2==0)
+                    pos+=2;
+                cout<<i<<" "<<pos<<" "<<nums[i]<<" odd\n";
+                int temp=nums[i];
+                nums[i]=nums[pos];
+                nums[pos]=temp;
+            }
+        }
+        return nums;
+    }
+};
