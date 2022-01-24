ab133ff2715ab1aff0a06b433ac5008ce9993bf8
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-01-24 18:45:23 +0545

    Time: 16 ms (87.14%), Space: 19.7 MB (50.30%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i:nums){
-            if (mp.find(i)==mp.end()){
-                mp[i] = 1;
-            }
-            else
-                mp[i]++;
+        //Boyer-Moore Voting Algorithm
+        
+        int count = 1, candidate=nums[0], n = nums.size();
+        for(int i=1;i<n;i++){
+            if (count==0)
+                candidate = nums[i];
+            count += (nums[i]==candidate)?1:-1;
         }
-        int max=INT_MIN, ele;
-        for(auto i:mp){
-            if (i.second>max){
-                max = i.second;
-                ele = i.first;
-            }
-        }
-        return ele;
+        return candidate;
+        
+        // unordered_map<int, int> mp;
+        // for(int i:nums){
+        //     if (mp.find(i)==mp.end()){
+        //         mp[i] = 1;
+        //     }
+        //     else
+        //         mp[i]++;
+        // }
+        // int max=INT_MIN, ele;
+        // for(auto i:mp){
+        //     if (i.second>max){
+        //         max = i.second;
+        //         ele = i.first;
+        //     }
+        // }
+        // return ele;
     }
 };
