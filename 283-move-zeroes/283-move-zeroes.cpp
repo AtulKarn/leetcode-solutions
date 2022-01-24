9cc894c60a96b9de41f60fba4d9ff2512de0b625
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-01-24 23:54:36 +0545

    Time: 40 ms (30.46%), Space: 19.3 MB (57.23%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int count=0;
-        for(int i=0;i<n-count;i++){
-            if (nums[i]==0){
-                for(int j=i;j<n-count-1;j++){
-                    swap(nums[j], nums[j+1]);
-                }
-                count++;
-                i--;
+        for(int lastNonZero = 0, current=0;current<n;current++){
+            if (nums[current]!=0){
+                swap(nums[lastNonZero++], nums[current]);
             }
-            
         }
+        
+        
+        //This approach works and passes all test cases but is not optimal
+//         int count=0;
+//         for(int i=0;i<n-count;i++){
+//             if (nums[i]==0){
+//                 for(int j=i;j<n-count-1;j++){
+//                     swap(nums[j], nums[j+1]);
+//                 }
+//                 count++;
+//                 i--;
+//             }    
+//         }
+        
+        
         
         //This approach works but changes the order of elements, so gives WA
         // int left = 0, right = n-1;
