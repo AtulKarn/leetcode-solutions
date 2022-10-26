aa8ab47dac8973556995d2263b7fa0070b068257
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-10-26 23:57:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,12 +3,14 @@ public:
     int climbStairs(int n) {
         if (n<4)
             return n;
-        vector<int> v(n+1, 0);
-        v[0] = 1;
-        v[1] = 1;
+        int a = 1;
+        int b = 1;
+        int c;
         for(int i=2;i<=n;i++){
-            v[i] = v[i-1] + v[i-2];
+            c = a+b;
+            a = b;
+            b = c;
         }
-        return v[n];
+        return c;
     }
 };
