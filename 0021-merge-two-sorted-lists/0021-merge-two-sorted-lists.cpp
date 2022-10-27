93d96668f8abf9cb06566d6b96af02417448675a
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-10-27 22:25:39 +0530

    Time: 11 ms (73.71%), Space: 14.7 MB (97.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -18,21 +18,40 @@ public:
         
         //recursive approach
         
+        // if (l1->val <= l2->val){
+        //     l1->next = mergeTwoLists(l1->next, l2);
+        //     return l1;
+        // }
+        // else{
+        //     l2->next = mergeTwoLists(l1, l2->next);
+        //     return l2;
+        // }
+        
+        
+        //iterative
+        
+        ListNode* head=l1;
         if (l1->val <= l2->val){
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
+            l1 = l1->next;       
         }
         else{
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
+            head = l2;
+            l2 = l2->next;
         }
-//         ListNode* head1 = l1;
-//         while(l1 & l2){
-//             if (l1->val < l2->val){
-//                 l1 = l1->next;       
-//             }
-//             else{
-//             }
-//         }
+        ListNode* current = head;
+        while(l1 && l2){
+            if (l1->val <= l2->val){
+                current->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                current->next = l2;
+                l2 = l2->next;
+            }
+            current = current->next;
+        }
+        if (!l1) current->next = l2;
+        else current->next = l1;
+        return head;
     }
 };
