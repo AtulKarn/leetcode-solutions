d267649c2226243aa80735ae7e4dd0a0caff7ad2
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-10-27 22:19:52 +0530

    Time: 19 ms (26.25%), Space: 14.8 MB (81.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,44 +10,29 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        if (!l1->next){
-            l1->next = l2;
-            return l1;
-        }
-        ListNode* current1 = l1, *next1 = l1->next;
-        ListNode* current2 = l2, *next2 = l2->next;
-        while(next1 && current2){
-            if ((current2->val >= current1->val) && (current2->val <= next1->val)){
-                next2 = current2->next;
-                current1->next = current2;
-                current2->next = next1;
-                
-                current1 = current2;
-                current2 = next2;
-            }
-            else{
-                if (next1->next){
-                    next1 = next1->next;
-                    current1 = current1->next;
-                }
-                else{
-                    next1->next = current2;
-                    return l1;
-                }
-            }
-        }
-        return l1;
-    }
-    
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if (l1 == NULL)
             return l2;
         else if (l2 == NULL)
             return l1;
-        if (l1->val < l2->val)
-            return merge(l1,l2);
-        else 
-            return merge(l2,l1);
+        
+        //recursive approach
+        
+        if (l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+//         ListNode* head1 = l1;
+//         while(l1 & l2){
+//             if (l1->val < l2->val){
+//                 l1 = l1->next;       
+//             }
+//             else{
+//             }
+//         }
     }
 };
