5e8242f197ea12675e8b0dfa54cd9165194035cd
Atul Kumar Karn <45361490+AtulKarn@users.noreply.github.com>
2022-10-27 16:53:19 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.66%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
+        // uint32_t ans = 0;
         // int i = 31;
         // while(n){
         //     ans += (n&1)*pow(2, i);
         //     n = n>>1;
         //     i--;
         // }
+        // return ans;
         
-        for(int i=0;i<32;i++){
-            ans <<=1;
-            ans += (n&1);
-            n >>= 1;
-        }
-        return ans;
+        // uint32_t ans = 0;
+        // for(int i=0;i<32;i++){
+        //     ans <<=1;
+        //     ans += (n&1);
+        //     n >>= 1;
+        // }
+        // return ans;
+        
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+        
     }
 };
